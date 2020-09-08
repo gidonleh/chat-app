1cc58f84347f6d797060e8f3aec427d7c666d23e
Gadi_Lehmann <csgadileh@gmail.com>
2020-09-08 16:29:44 +0300

    Updates

---
+++ precache-manifest.bd297fdce2bf4e1aa819cf6e0292dd59.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c837dfb28366d0bb94a1c12bdedbdd42",
+    "url": "/chat-app/index.html"
+  },
+  {
+    "revision": "90d666aa30bf9b1baece",
+    "url": "/chat-app/static/css/main.0c62acd2.chunk.css"
+  },
+  {
+    "revision": "475c6270f8b2b8e65589",
+    "url": "/chat-app/static/js/2.1bffd975.chunk.js"
+  },
+  {
+    "revision": "0aee8186d6749ad5d8cab044b9c4b878",
+    "url": "/chat-app/static/js/2.1bffd975.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90d666aa30bf9b1baece",
+    "url": "/chat-app/static/js/main.ce6775b4.chunk.js"
+  },
+  {
+    "revision": "a46df673de3fe3887809",
+    "url": "/chat-app/static/js/runtime-main.0c4eda3f.js"
+  }
+]);
